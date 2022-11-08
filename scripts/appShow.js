0aa456fb7b1aeae28db8950e585c1ffd5f20ce4c
FerVid <116863002+FerVid1507@users.noreply.github.com>
2022-11-07 21:01:03 -0600

    Add files via upload

--- scripts/appShow.js
+++ scripts/appShow.js
@@ -15,33 +15,15 @@ function cargar() {
             document.querySelector('#Contenedor').innerHTML += `
 
 
-
-
-            <div class="col" style="opacity:0.95;">
-           <div class="p-3 border bg-light" style="border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214)">
-        
-            <div class="card" style="opacity:0.95" width: 20rem; border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214);>
-              <div class="card-header" style="text-align: center;">
-              ${datos[indice].titulo}
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"></h5>
-                <h6 class="card-text">${datos[indice].contenido}</h6>
-                <p class="card-subtitle mb-2 text-muted" style="margin-top:10px;">Autor: ${datos[indice].autor}</p>
-              </div>
-            </div>  
-          </div>
-          </div>
-
-
-            <div class="col" style='height: 10px; opacity:0'>
-            <div class="col-4">
-              <div class="card" style="width: 20rem; border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214)">
-                <div class="card-body">
-                </div>
-              </div>
+            <div class="col-sm-6" style="padding-bottom:10px;">
+          <div class="card" style="border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214)">
+            <div class="card-body">
+              <h5 class="card-title"> ${datos[indice].titulo}</h5>
+              <p class="card-text">Autor: ${datos[indice].contenido}</p>
+              <p class="card-subtitle mb-2 text-muted" style="margin-top:10px;">Autor: ${datos[indice].autor}</p>
             </div>
           </div>
+        </div>
    
             `;
 
