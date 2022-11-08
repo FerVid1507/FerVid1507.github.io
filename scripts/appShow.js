4367469e0165606514de05d26c800080463b6981
FerVid <116863002+FerVid1507@users.noreply.github.com>
2022-11-07 20:49:09 -0600

    Add files via upload

--- scripts/appShow.js
+++ scripts/appShow.js
@@ -14,13 +14,30 @@ function cargar() {
 
             document.querySelector('#Contenedor').innerHTML += `
 
-            <div class="col" style='margin-bottom: 5px; margin-top:5px;'>
+
+
+
+            <div class="col" style="opacity:0.95;">
+           <div class="p-3 border bg-light" style="border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214)">
+        
+            <div class="card" style="opacity:0.95" width: 20rem; border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214);>
+              <div class="card-header" style="text-align: center;">
+              ${datos[indice].titulo}
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"></h5>
+                <h6 class="card-text">${datos[indice].contenido}</h6>
+                <p class="card-subtitle mb-2 text-muted" style="margin-top:10px;">Autor: ${datos[indice].autor}</p>
+              </div>
+            </div>  
+          </div>
+          </div>
+
+
+            <div class="col" style='height: 10px; opacity:0'>
             <div class="col-4">
               <div class="card" style="width: 20rem; border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214)">
                 <div class="card-body">
-                  <h4 class="card-title" style=' text-align: center;'>${datos[indice].titulo}</h4>
-                  <p class="card-text" style=' size: 20px; text-align: justify;'>${datos[indice].contenido}</p>
-                  <p class="card-subtitle mb-2 text-muted">Autor: ${datos[indice].autor}</p>
                 </div>
               </div>
             </div>
