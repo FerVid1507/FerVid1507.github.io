e8955e6a6a7149153713e3613dea141cf52130d9
FerVid <116863002+FerVid1507@users.noreply.github.com>
2022-11-08 20:31:25 -0600

    Add files via upload

--- scripts/appShow.js
+++ scripts/appShow.js
@@ -18,8 +18,14 @@ function cargar() {
             <div class="col-sm-6" style="padding-bottom:10px;">
           <div class="card" style="border-radius: 10px; opacity:0.95; background-color:rgb(216, 214, 214)">
             <div class="card-body">
-              <h5 class="card-title" style="text-align: center; font-family:'Fredoka One';"> ${datos[indice].titulo}</h5>
-              <p class="card-text" style=" font-family:'Fredoka', sans-serif; text-align: justify;">Autor: ${datos[indice].contenido}</p>
+              <h5 class="card-title" style="text-align: center; font-family:'Fredoka One'; font-size: 24px; "> ${datos[indice].titulo}</h5>
+
+              <img src="images/quote.png" style="width: 30px; margin-bottom:-10px;"/>
+
+              <p class="card-text" style="color:#616161; font-family:'Fredoka', sans-serif; text-align: justify; font-size: 20px; ">"${datos[indice].contenido}"</p>
+
+              <img src="images/double_quotes.png" style="width: 30px; float:right; margin-top: -25px;"/>
+
               <p class="card-subtitle mb-2 text-muted" style="margin-top:10px; font-family:'Pacifico';">Autor: ${datos[indice].autor}</p>
             </div>
           </div>
