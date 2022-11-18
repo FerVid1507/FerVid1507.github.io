570cd43b21d182a9ea0326b6eccbfa6b6c380d6d
FerVid <116863002+FerVid1507@users.noreply.github.com>
2022-11-17 19:51:41 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -13,26 +13,27 @@ let botonRecargar = document.getElementById('btnRecargar');
 
 
 
-let saldoCurrent = '';
+let saldoCurrent = 0;
 
-botonRecargar.addEventListener('click', ()=>{
+botonRecargar.addEventListener('click', () => {
     var date = new Date();
     let newSaldo = parseInt(saldo.textContent.slice(1)) + parseInt(txtRecargar.value);
     console.log('Recarga: ' + txtRecargar.value);
-    if(txtRecargar.value!=0){
-        db.collection('saldo').add({
-            date: date.toUTCString(),
+    if (txtRecargar.value != 0) {
+        db.collection('saldo').doc(date.toUTCString()).set({
+            //date: date.toUTCString(),
+            date: date.toLocaleString() + '',
             saldo: newSaldo,
             recarga: txtRecargar.value,
             type: 'Recarga'
         }).then((docRef) => {
-            console.log('Document: ' + docRef.id);
+            console.log('Event sucessfuly!');
             readSaldo();
         }).catch((error) => {
             console.log('Error: ' + error);
         });
     }
-    
+
 });
 
 
@@ -43,55 +44,49 @@ btnPay.addEventListener('click', () => {
     alert(pasajes.textContent);
 });
 
-async function readSaldo() {
-    datos = [];
-    try {
-        const res = await getItems();
-        datos = [...res];
-    } catch (error) { }
-}
+function readSaldo() {
 
-async function getItems() {
-    let items = [];
-    let html = '';
+    let saldoCurrent = 0;
+    //const response = await db.collection('saldo').orderBy('date', 'desc').get();
 
-    const response = await db.collection('saldo').orderBy('date', 'desc').get();
-    let flag = false;
-    response.forEach(element => {
-        items.push(element.data());
-        console.log(element.data().type + ' ' + element.data().date + ' $' + element.data().saldo);
-        
-        if(!flag)saldoCurrent = element.data().saldo;
-        flag = true;
-        html += `
+
+    db.collection('saldo').onSnapshot((doc) => {
+        let items = [];
+        document.querySelector('#Contenedor').innerHTML = '';
+        doc.forEach(element => {
+            items.push({ ...element.data() });
+        });
+
+        items.map(
+            function (element) {
+                saldoCurrent = element.saldo;
+                document.querySelector('#Contenedor').innerHTML += `
         <li class="list-group-item d-flex justify-content-between align-items-start">
             <div class="ms-2 me-auto">
-            <div class="fw-bold">${element.data().date}</div>
-            Tipo: ${element.data().type}
+            <div class="fw-bold">${element.date}</div>
+            Tipo: ${element.type}
          </div>
-            <span class="badge bg-primary rounded-pill">$${element.data().recarga}</span>
+            <span class="badge bg-primary rounded-pill">$${element.recarga}</span>
         </li>
         `;
+            });
+
+            console.log(saldoCurrent);
+              saldo.innerText = '$'+saldoCurrent;
+              let numPasajes = parseInt(saldoCurrent / 4.75);
+          
+              if(numPasajes!=0){
+                  pasajes.innerText = numPasajes;
+              }else{
+                  pasajes.innerText = 'Realiza una recarga!';
+              }
+
     });
-    document.querySelector('#Contenedor').innerHTML = html;
-    //let newSaldo = parseInt(saldo.textContent.slice(1)) + parseInt(saldoCurrent);
-    console.log(!Number.isNaN(saldoCurrent));
-    if(!Number.isNaN(saldoCurrent)){
-    
-    saldo.innerText = '$'+saldoCurrent;
-    let numPasajes = parseInt(saldoCurrent / 4.75);
-
-    if(numPasajes!=0){
-        pasajes.innerText = numPasajes;
-    }else{
-        pasajes.innerText = 'Realiza una recarga!';
-    }
-    
-    }else{
-        saldo.innerText = '$0';
-        numPasajes.innerText = '0';
-    }
-
 
     
+   
+    
+      
+
+
 }
